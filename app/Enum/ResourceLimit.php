86f343981c0e3fe31e572b726796161880011bfb
vanes430 <vanessimbolon2020@gmail.com>
2026-02-26 12:43:26 +0000

    feat: increase FilePull resource limit to 15 requests per minute

--- app/Enum/ResourceLimit.php
+++ app/Enum/ResourceLimit.php
@@ -46,7 +46,7 @@ enum ResourceLimit
             self::Allocation => Limit::perMinute(10),
             self::Backup => Limit::perMinutes(15, 3),
             self::Database => Limit::perMinute(2),
-            self::FilePull => Limit::perMinutes(10, 5),
+            self::FilePull => Limit::perMinute(15),
             self::Subuser => Limit::perMinutes(15, 10),
             self::Websocket => Limit::perMinute(60),
             default => Limit::perMinute(2),
